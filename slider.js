dc27aff75976cda0db796ebd499649b5aab5c585
Сергій Заморило <zamorylo@i.ua>
2024-01-14 21:38:10 +0200

    .

--- slider.js
+++ slider.js
@@ -2,11 +2,8 @@ const container=document.querySelector('.sliderCont')
 const back=document.querySelector('#backButton')
 const next=document.querySelector('#nextButton')
 const img=document.querySelector('#img')
-let imgNow = 0
-const imges=[
-    'Image1.jpg',
-    'Image2.jpg',
-    'Image3.jpg']
+let imgNow =0
+const imges=['Image1.jpg','Image2.jpg','Image3.jpg']
 next.addEventListener('click',()=>{
     if(imgNow==imges.length-1){
         imgNow=0
@@ -14,9 +11,9 @@ next.addEventListener('click',()=>{
         imgNow+=1
     }
     img.src=`/img/${imges[imgNow]}`;
-})
+}),
 back.addEventListener('click',()=>{
-    if(imgNow=imges.length=0){
+    if(imgNow=imges.length==0){
         imgNow=imges.length-1
     } else{
         imgNow-=1
